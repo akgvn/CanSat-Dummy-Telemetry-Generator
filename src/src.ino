88bece38cd90c2892732a1435a55ab56b70ecda8
akgvn <akgvn@users.noreply.github.com>
2020-02-01 16:58:54 +0300

    Added initial source files, nothing in them yet.

---
+++ src/src.ino
@@ -0,0 +1,13 @@
+// CanSat Dummy telemetry generator
+// Abdulkerim Güven, 2020-02
+
+// TODO: Everything.
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
